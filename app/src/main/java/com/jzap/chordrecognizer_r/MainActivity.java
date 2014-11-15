25101166e0d1e25ee427ed93be1b374a23664ccf
Justin <jzortea@gmail.com>
2014-11-15 11:08:53 -0500

    Created layout, setup MainActivity infrastrcture, added blank MainWorkerRunnable class

--- app/src/main/java/com/jzap/chordrecognizer_r/MainActivity.java
+++ app/src/main/java/com/jzap/chordrecognizer_r/MainActivity.java
@@ -2,18 +2,31 @@ package com.jzap.chordrecognizer_r;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "MainActivity";
+
+    TextView mTv_chord, mTv_mostIntenseNote, mTv_secMostIntenseNote, mTv_thirdMostIntenseNote;
+    RadioButton mRb_recording;
+    Switch mSwitch_autoDetect;
+
+// Activity Overrides
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        initializeMembers();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +49,43 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+// End Activity Overrides
+
+    private void initializeMembers() {
+        mTv_chord = (TextView) findViewById(R.id.tv_chord);
+        mTv_mostIntenseNote = (TextView) findViewById(R.id.tv_mostIntenseNote);
+        mTv_secMostIntenseNote = (TextView) findViewById(R.id.tv_secMostIntenseNote);
+        mTv_thirdMostIntenseNote = (TextView) findViewById(R.id.tv_thirdMostIntenseNote);
+        mRb_recording = (RadioButton) findViewById(R.id.rb_recording);
+        mSwitch_autoDetect = (Switch) findViewById(R.id.switch_autoDetect);
+    }
+
+// Accessors/Mutators
+    public TextView getmTv_chord() {
+        return mTv_chord;
+    }
+
+    public TextView getmTv_mostIntenseNote() {
+        return mTv_mostIntenseNote;
+    }
+
+    public TextView getmTv_secMostIntenseNote() {
+        return mTv_secMostIntenseNote;
+    }
+
+    public TextView getmTv_thirdMostIntenseNote() {
+        return mTv_thirdMostIntenseNote;
+    }
+
+    public RadioButton getmRb_recording() {
+        return mRb_recording;
+    }
+
+    public Switch getmSwitch_autoDetect() {
+        return mSwitch_autoDetect;
+    }
+// End Accessors/Mutators
+
+
+
 }
---
+++ app/src/main/java/com/jzap/chordrecognizer_r/MainWorkerRunnable.java
@@ -0,0 +1,7 @@
+package com.jzap.chordrecognizer_r;
+
+/**
+ * Created by Justin on 11/15/2014.
+ */
+public class MainWorkerRunnable {
+}
